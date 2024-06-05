39a889352f32744a39a0127d2c9f22e5f19d9dd6
Jot43 <139986366+Jot43@users.noreply.github.com>
2024-06-05 08:07:23 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -189,8 +189,8 @@ def progress_bar(pct):
         pct = float(pct.strip('%'))
     p = min(max(pct, 0), 100)
     cFull = int((p + 5)// 10)
-    p_str = '■' * cFull
-    p_str += '□' * (10 - cFull)
+    p_str = '●' * cFull
+    p_str += '○' * (10 - cFull)
     return p_str
 
 
@@ -199,7 +199,7 @@ def source(self):
 
 
 def get_readable_message():
-    msg = '<b>           @NordBotz</b>\n\n'
+    msg = '<b>~           @NordBotz</b>\n\n'
     button = None
     tasks = len(download_dict)
     currentTime = get_readable_time(time() - botStartTime)
